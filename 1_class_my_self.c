5764a2b12533308493e73030083cdfd2bd5c165b
Sofiya288 <mdtehsinkhantehsinkhan@gmail.com>
2026-06-07 12:48:41 +0530

    Create 1_class_my_self.c

---
+++ 1_class_my_self.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("My name is Sofiya.\n");
+    printf("I study in 9th class.\n");
+    printf("I am 14 years old.\n");
+    printf("My favourite colour is Black.\n");
+    printf("My favourite fruit is watermelon.\n");
+    printf("My hobby is reading.\n");
+    printf("My father name is MD tehseen.\n");
+    printf("My mother name is Aayesha.\n");
+}
