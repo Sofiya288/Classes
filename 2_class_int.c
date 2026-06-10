25bd02b690d61ec41503ae33999430a1b6463012
Sofiya288 <mdtehsinkhantehsinkhan@gmail.com>
2026-06-10 22:09:40 +0530

    Create 2_class_int.c

---
+++ 2_class_int.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int number = 3;    
+    int month = 6;
+    int year = 2026;
+    int birthday_date = 22;
+    int birth_year = 2012;
+    int date = 10;
+    int favourite_no = 288;
+    printf("number: %d\n", number);
+    printf("month: %d\n", month);
+    printf("year: %d\n", year);
+    printf("birthday_date: %d\n", birthday_date);
+    printf("birth_year: %d\n", birth_year);
+    printf("date: %d\n",date);
+    printf("favourite_no : %d\n", favourite_no);
+}    
